c27eb6c69046ab9fec22aab90f6eb0c8c8882218
anatoly <tolikybm@gmail.com>
2022-12-24 13:26:02 +0300

    1

--- controllers/DayEventController.tsx
+++ controllers/DayEventController.tsx
@@ -1,5 +1,38 @@
+import fs from 'fs';
 import DayEvent from '../models/DayEvent';
 import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const exportFromCsv = async () => {
+    const end = new Promise((resolve, reject) => {
+        const results: any[] = [];
+        fs.createReadStream(__dirname + "/../../../../1.csv")
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                resolve(results);
+            });
+    });
+
+    const data: any = await end;
+
+    await DayEvent.deleteMany({});
+
+    for (let i = 0; i < data.length; i++) {
+        console.log(data[i]["Дата"]);
+        const dayEvent = new DayEvent();
+
+        dayEvent.date = `${data[i]["Дата"].split('/')[0]}/${data[i]["Дата"].split('/')[1]}`,
+            dayEvent.category = getCategory(data[i]["Категория"]),
+            dayEvent.serviceType = getServiceType(data[i]["Служба"]),
+            dayEvent.saintType = getSaintType(data[i]["Ранг"]),
+            title_en: data[i]["Name"],
+                title_ru: data[i]["Имя"],
+                    dayEvent.year = data[i]["Год"],
+
+    }
+
+    return "Done";
+};
 
 export const createDayEventData = async (req: NextApiRequest) => {
     const { body } = req;
